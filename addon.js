481f8afa72ec30ed5b69f78141f87f404a018b55
郑聪 <931745526@qq.com>
2017-09-29 10:13:16 +0800

    optimize code

---
+++ addon.js
@@ -0,0 +1,11 @@
+const { readFile, writeFile } = require('./src/native/build/Release/addon.node');
+const buf = Buffer.from('Hello world!');
+// File path can change to what you want
+const filePath = 'D:/Users/w13269464702/Desktop/addon.txt';
+
+// Create a file named addon.txt
+writeFile(filePath, buf);
+const content = readFile(filePath);
+// Reading file content, content is buffer type
+console.log(content);
+
